e4a35d5f349d60a6864f507b9f5bfbda367917e0
okoala <dapixp@gmail.com>
2018-10-02 17:24:26 +0800

    fea: swagger-ui work

--- app.js
+++ app.js
@@ -1,29 +1,24 @@
-'use strict';
+"use strict";
 
-const swaggerLoader = require('./lib/swagger_loader');
-const swaggerUI = require('./lib/swagger_ui');
-const SwaggerRouter = require('./lib/swagger_router');
+const swaggerLoader = require("./lib/swagger_loader");
+const swaggerUI = require("./lib/swagger_ui");
+const SwaggerRouter = require("./lib/swagger_router");
 
-module.exports = class {
-  constructor(app) {
-    this.app = app;
+module.exports = app => {
+  app.swagger = swaggerLoader(app);
+
+  if (app.config.swaggerbox.ui.enable) {
+    swaggerUI(app);
   }
 
-  async didLoad() {
-    const { app } = this;
-    app.swagger = swaggerLoader(app);
-
-    if (app.config.swaggerbox.ui.enable) {
-      swaggerUI(app);
-    }
-
-    if (app.config.swaggerbox.router.enable) {
+  if (app.config.swaggerbox.router.enable) {
+    app.beforeStart(async () => {
       const swaggerRouter = new SwaggerRouter(
         app,
         app.config.swaggerbox.router
       );
 
       await swaggerRouter.init();
-    }
+    });
   }
 };
